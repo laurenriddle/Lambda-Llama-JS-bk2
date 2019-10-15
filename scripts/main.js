d5147f75b4f3c8fe6941a09e5fca2d0ea90187be
Lauren Riddle <lari7132@gmail.com>
2019-10-15 11:44:32 -0500

    fixed the Lambda Llama function so that it will return a random name

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+console.log("Test!")
+
+
+const llamaNamer = function () {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7)
+
+    const namer = function () {
+        const suffix = " the Llama"
+        const name = possibleNames[randomizer]
+        return name + suffix
+    }
+
+    return namer()
+    
+}
+
+nameMaker = llamaNamer()
+console.log(nameMaker)
